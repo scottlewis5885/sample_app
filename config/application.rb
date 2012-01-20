d206824a3b085559c08933a685153925282d622f
Scott Lewis <scott@salewis.org>
2012-01-20 05:47:33 -0800

    precompiled screen.css

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,6 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += ['blueprint/screen.css']
+    config.assets.precompile += ['blueprint/screen.css','blueprint/print.css']
   end
 end
